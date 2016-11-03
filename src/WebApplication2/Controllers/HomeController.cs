122128f578e71e897c67e1cb1e1f50ef364e26fa
jklin <jklin@THESURFACE>
2016-11-03 11:02:18 +0100

    Add project files.

---
+++ src/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            var viewModel = new CompanyBoxVM
+            {
+                CompanyName = "Acme",
+                City = "Malmö"
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApplication2/Models/CompanyBoxVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class CompanyBoxVM
+    {
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
